b4a97dcbe169af3b252c812a7842faa518bfbbd9
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-03-21 20:12:50 +0100

    use async/await for blog

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,34 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/Blog');
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs);
-        });
+blogsRouter.get('/', async (req, res) => {
+    try {
+        const blogs = await Blog.find({});
+
+        if(blogs) {
+            res.json(blogs);
+        }
+        else {
+            res.status(404).end();
+        }
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).end();
+    }
 });
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body);
+blogsRouter.post('/', async (req, res) => {
+    const blog = new Blog(req.body);
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result);
-        });
+    try {
+        const result = await blog.save();
+        res.status(201).json(result);
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).end();
+    }
 });
 
 module.exports = blogsRouter;
